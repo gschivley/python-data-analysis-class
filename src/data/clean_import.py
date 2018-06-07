48e609adcab6e991ebe95371c87babe701d0dc40
gschivley <gschivley@users.noreply.github.com>
2018-06-07 15:50:01 -0400

    Add (and use) functions to find max capacity and max generation

--- src/data/clean_import.py
+++ src/data/clean_import.py
@@ -33,6 +33,26 @@ def import_epa_emissions(path):
     return df
 
 
+def find_max_tech_capacity(df, group):
+
+    max_tech = {'plant_id': [],
+                'technology': []}
+
+    for plant, _df in df.groupby(group):
+        max_tech['plant_id'].append(plant)
+
+        try:
+            tech = _df.loc[_df['summer_capacity_mw'].idxmax(), 'technology']
+        except TypeError:
+            tech = _df.loc[_df['nameplate_capacity_mw'].idxmax(),
+                           'technology']
+        max_tech['technology'].append(tech)
+
+    max_tech_df = pd.DataFrame(max_tech)
+
+    return max_tech_df
+
+
 def import_plant_capacity(path):
     """
     Read and clean the EIA 860 monthly file with capacity data.
@@ -64,9 +84,12 @@ def import_plant_capacity(path):
 
     # Group data to facility level. Use state in the grouping to keep the data.
     df_grouped = df.groupby(['plant_id', 'state'], as_index=False).sum()
-    # df_grouped = df_grouped.merge(df['state'], on='plant_id')
 
-    return df_grouped
+    # Add the technology of maximum capacity for each plant
+    max_capacity_tech = find_max_tech_capacity(df, 'plant_id')
+    final_df = pd.merge(df_grouped, max_capacity_tech, on='plant_id')
+
+    return final_df
 
 
 def import_plant_generation(path):
@@ -81,7 +104,7 @@ def import_plant_generation(path):
     Returns
     -------
     DataFrame
-        Clean dataframe of plant generation
+        Clean dataframe of plant generation and primary generation fuel
 
     """
 
@@ -97,18 +120,53 @@ def import_plant_generation(path):
 
     gen_cols = [col for col in df.columns if 'netgen' in col]
 
-    df = (pd.melt(df, id_vars=['plant_id'],
-                  value_vars=gen_cols, value_name='net_gen_mwh',
-                  var_name='month')
-            .groupby(['plant_id', 'month'],
-                     as_index=False).sum())
+    # Melt generation data so it is stacked vertically rather than in columns
+    # Keep generation by plant and fuel type to find the primary fuel
+    df_tidy = pd.melt(df, id_vars=['plant_id', 'aer_fuel_type_code'],
+                      value_vars=gen_cols, value_name='net_gen_mwh',
+                      var_name='month')
 
-    df['month'] = df.month.str.replace('netgen_', '')
+    # Find primary fuel, group tidy data to plant level, merge in primary fuel
+    primary_fuel = find_primary_gen_fuel(df_tidy)
+    df_tidy = df_tidy.groupby(['plant_id', 'month'], as_index=False).sum()
+    df_tidy = df_tidy.merge(primary_fuel, on='plant_id')
+
+    df_tidy['month'] = df_tidy.month.str.replace('netgen_', '')
 
     # Change month from names to integer values and re-sort
     month_map = {month: idx for idx, month in enumerate(calendar.month_name)}
-    df['month'] = df.month.str.capitalize().map(month_map)
-    df.sort_values(by=['plant_id', 'month'], inplace=True)
-    df.reset_index(drop=True, inplace=True)
+    df_tidy['month'] = df_tidy.month.str.capitalize().map(month_map)
+    df_tidy.sort_values(by=['plant_id', 'month'], inplace=True)
+    df_tidy.reset_index(drop=True, inplace=True)
 
-    return df
+    return df_tidy
+
+
+def find_primary_gen_fuel(df):
+    """
+    Determine which fuel generated the most electricity at each power plant
+    in 2016
+
+    Parameters
+    ----------
+    df : dataframe
+        Dataframe with monthly generation by facility and fuel type
+
+    Returns
+    -------
+    DataFrame
+        Clean dataframe of primary generation fuel at each plant
+    """
+    # Sum the generation for each plant by fuel across the entire year
+    df_fuel = (df.groupby(['plant_id', 'aer_fuel_type_code'], as_index=False)
+                 .sum())
+
+    # Find the dataframe index for the fuel with the most gen at each plant
+    # Use this to slice the dataframe and return plant code and primary fuel
+    primary_fuel_idx = df_fuel.groupby('plant_id')['net_gen_mwh'].idxmax()
+    primary_fuel = df_fuel.loc[primary_fuel_idx,
+                               ['aer_fuel_type_code', 'plant_id']]
+
+    primary_fuel.rename(columns={'aer_fuel_type_code': 'primary_gen_fuel'})
+
+    return primary_fuel
